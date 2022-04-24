3470df0b541470b2ee9418ec4ad6c28851f25999
mehdiec <mehdiechh@gmail.com>
2022-04-24 18:02:24 +0200

    [+] added cnn

--- src/models.py
+++ src/models.py
@@ -8,10 +8,59 @@ import timm
 from nptyping import NDArray
 
 
+def dropout_linear_relu(dim_in, dim_out, p_drop):
+    return [nn.Dropout(p_drop), nn.Linear(dim_in, dim_out), nn.ReLU(inplace=True)]
+
+
+def conv_relu_maxp(in_channels, out_channels, ks):
+    return [
+        nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=ks,
+            stride=1,
+            padding=int((ks - 1) / 2),
+            bias=True,
+        ),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=2),
+    ]
+
+
+class VanillaCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(VanillaCNN, self).__init__()
+
+        # By default, Linear layers and Conv layers use Kaiming He initialization
+
+        self.features = nn.Sequential(
+            *conv_relu_maxp(1, 16, 5),
+            *conv_relu_maxp(16, 32, 5),
+            *conv_relu_maxp(32, 64, 5)
+        )
+        probe_tensor = torch.zeros((1, 1, 28, 28))
+        out_features = self.features(probe_tensor).view(-1)
+
+        self.classifier = nn.Sequential(
+            *dropout_linear_relu(out_features.shape[0], 128, 0.5),
+            *dropout_linear_relu(128, 256, 0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size()[0], -1)  #  OR  x = x.view(-1, self.num_features)
+        y = self.classifier(x)
+        return y
+
+
 def build_model(model_name, num_classes):
     model = None
 
-    if model_name == "resnet":
+    if model_name == "vanilla":
+        model = VanillaCNN(num_classes)
+
+    elif model_name == "resnet":
         resnet = timm.create_model("resnet18", pretrained=True)
         infeat = resnet.fc.in_features
         resnet.fc = nn.Linear(infeat, num_classes)
