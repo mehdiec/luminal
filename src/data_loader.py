7f4ca2acbed6aa4a297e5c8afb161e8a108dfcb2
mehdi <mehdiechh@gmail.com>
2022-04-29 10:21:18 +0200

    [+] added notes and doc string

--- src/data_loader.py
+++ src/data_loader.py
@@ -25,53 +25,72 @@ class ClassificationDataset(Dataset):
     def __init__(
         self,
         slide_file: str,
+        outfolder: Path = Path("/data/DeepLearning/mehdi"),
         transforms: Optional[Sequence[BasicTransform]] = None,
         slide_backend: str = "cucim",
-        min_size: int = 10,
         split: str = "train",
         noted: bool = False,
     ):
+        """_summary_
+
+        Args:
+            slide_file (str): file name with its full path containing the data split
+            outfolder (Path, optional): folder containing all the data used in the loader. Defaults to Path("/data/DeepLearning/mehdi").
+            transforms (Optional[Sequence[BasicTransform]], optional): image transformation used for data augmentation. Defaults to None.
+            slide_backend (str, optional): how we chose to open our slides with. Defaults to "cucim".
+            split (str, optional): the split to load. Defaults to "train".
+            noted (bool, optional): chose whether to load the slide noted or not. Defaults to False.
+        """
+
         super().__init__()
+        # info retrieved from the csv
         self.slides = []
         self.masks = []
         self.patches = []
-        self.slide_idxs = []
         self.labels = []
         self.split = []
+        # info added to identify each patch to e=its coresponding slide
+        self.slide_idxs = []
+
         slide_idx = 0
         self.noted = noted
+
         with open(slide_file, "r") as out_file:
             reader = csv.DictReader(out_file)
 
-            for row in reader:
+            for (
+                row
+            ) in (
+                reader
+            ):  # we read each row of our csv to get the right slide for the right split
                 if row["split"] == split:
                     slide_path = row["id"]
 
-                    outfolder = Path("/data/DeepLearning/mehdi/csv/")
+                    outfolder = outfolder / "csv"
                     if self.noted:
-                        outfolder = Path("/data/DeepLearning/mehdi/csv_annot/")
+                        outfolder = outfolder / "csv_annot"
+
+                    self.slides.append(Slide(slide_path, backend=slide_backend))
+
+                    # we have access to the csv containing all the patches for a given slide
                     csv_file = Path(slide_path.split(sep="/")[-1][:-4])
                     patches_path = (
                         outfolder / "patch_csvs" / csv_file.with_suffix(".csv")
                     )
-                    self.slides.append(Slide(slide_path, backend=slide_backend))
 
-                    with open(
-                        patches_path, "r"
-                    ) as patch_file:  # maybe change since i have got the full paths
+                    with open(patches_path, "r") as patch_file:
                         reader = csv.DictReader(patch_file)
                         for patch in reader:
                             self.patches.append(Patch.from_csv_row(patch))
-
                             self.slide_idxs.append(slide_idx)
                             self.labels.append(MAPPING[row["ab"]])
                     slide_idx += 1
+
                     # delete
                     # if slide_idx == 1:
                     #     break
-
         self.transforms = Compose(ifnone(transforms, []))
-        self.min_size = min_size
+
         # self.clean()
 
     def __len__(self):
@@ -83,7 +102,7 @@ class ClassificationDataset(Dataset):
         patch = self.patches[idx]
         slide_idx = self.slide_idxs[idx]
         slide = self.slides[slide_idx]
-        target = self.labels[idx]  # to define
+        target = self.labels[idx]
 
         slide_region = np.asarray(
             slide.read_region(patch.position, patch.level, patch.size).convert("RGB"),
@@ -100,18 +119,3 @@ class ClassificationDataset(Dataset):
         }
 
         return image_with_slide_idx  # .transpose(2, 0, 1)
-
-    # def clean(self):
-    #     print(len(self[0]))
-    #     patches = []
-    #     slide_idxs = []
-    #     idxs = []
-    #     for i in range(len(self)):
-    #         print(i)
-    #         if self[i] is not None:
-    #             patches.append(self.patches[i])
-    #             slide_idxs.append(self.slide_idxs[i])
-    #             idxs.append(i)
-
-    #     self.patches = patches
-    #     self.slide_idxs = slide_idxs
